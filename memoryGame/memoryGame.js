7436a35dad201507899c76c4b4746020b85b2858
xiaoM <526371075@qq.com>
2017-11-16 22:32:39 +0800

    define info array
    define info array

--- memoryGame/memoryGame.js
+++ memoryGame/memoryGame.js
@@ -1,6 +1,7 @@
 !function (window, $) {
     const POSITION_ARRAY = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     const ICON_INDEX_ARRAY = [0, 1, 2, 3, 4, 5, 6, 7];
+    const WEATHER_ICON_ARRAY = ['wind', 'cat', 'dog', 'sun', 'rain', 'moon', 'lightning', 'tree'];
     /**
      * 
      */
